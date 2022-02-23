785c2fa9b497a8f9fd50f4b193c3808dbe364a5c
bruks <jenake8@gmail.com>
2022-02-23 19:25:05 +0100

    async

--- fantasyAsync.py
+++ fantasyAsync.py
@@ -1,6 +1,6 @@
 import json
 import time
-import requests
+import httpx
 import datetime
 import asyncio
 
@@ -13,34 +13,44 @@ class fantasySpider:
 		self.manager_history="https://fantasy.premierleague.com/api/entry/{}/history/"
 		self.manager_details="https://fantasy.premierleague.com/api/entry/{}/"
 		
-	def get_total_managers(self):
-		return int(json.loads(requests.get(self.general_info).text["total_players"]))
+	async def get_total_managers(self):
+		async with httpx.AsyncClient() as client:
+			r = await client.get(self.general_info).text["total_players"]
+			return r.text
 	
-	def get_history(self, manager):
-		return json.loads(requests.get(self.manager_history.format(manager)).text)
+	async def get_history(self, manager):
+		async with httpx.AsyncClient() as client:
+			r = await client.get(self.manager_history.format(manager))
+			return json.loads(r.text)
 	
-	def get_current_rank(self, manager):
+	async def get_current_rank(self, manager):
 		try:
-			return int(self.get_history(manager)["current"][-1]["overall_rank"])
+			history = await self.get_history(manager)
+			return int(history["current"][-1]["overall_rank"])
 		except:
-			return None
+			return 0
 
-	def get_previous_rank(self, manager):
-		return int(self.get_history(manager)["current"][-2]["overall_rank"])
+	async def get_previous_rank(self, manager):#
+		try:
+			history = await self.get_history(manager)
+			return int(history["current"][-2]["overall_rank"])
+		except:
+			return 0
+		
 
 	
-	def get_change_in_rank(self, manager):
-		change= self.get_current_rank(manager) - self.get_previous_rank(manager)
+	async def get_change_in_rank(self, manager):
+		change= await self.get_current_rank(manager) - await self.get_previous_rank(manager)
 		print(manager, change)
 		return change
 	
-	def get_manager_details(self, manager):
+	async def get_manager_details(self, manager):
 		updated= datetime.datetime.now()
-		data= json.loads(requests.get(self.manager_details.format(manager)).text)
+		data= json.loads(httpx.get(self.manager_details.format(manager)).text)
 		first_name= data["player_first_name"]
 		last_name= data["player_last_name"]
 		current_rank= data["summary_overall_rank"]
-		previous_rank= self.get_previous_rank(manager)
+		previous_rank= await self.get_previous_rank(manager)
 		
 		manager_data={
 			"updated": updated,
@@ -52,20 +62,23 @@ class fantasySpider:
 		return manager_data		
 		
 	
-	def get_max_mover_and_faller(self, start, stop):
+	async def get_max_mover_and_faller(self, start, stop):
 		updated= datetime.datetime.now()
+		"""
 		def yield_managers_change_in_rank():
-			for manager in range(start, stop+1):
+			for manager in range(start, stop):
 				yield self.get_change_in_rank(str(manager))
+		"""
+		change_in_ranks= await asyncio.gather(*map(self.get_change_in_rank, range(start, stop+1)))
 		
-		change_in_rank_list= [ change_in_rank for change_in_rank in  yield_managers_change_in_rank()  ]
+		change_in_rank_list= [ change_in_rank for change_in_rank in  change_in_ranks  ]
 		max_rise= min(change_in_rank_list)
 		max_riser_id=(change_in_rank_list.index(max_rise)) + start
 		min_rise= max(change_in_rank_list)
 		min_riser_id= (change_in_rank_list.index(min_rise)) + start
 		
-		max_mover_data= self.get_manager_details(max_riser_id)
-		max_faller_data= self.get_manager_details(min_riser_id)
+		max_mover_data= await self.get_manager_details(max_riser_id)
+		max_faller_data= await self.get_manager_details(min_riser_id)
 		
 		data= {
 			"updated": updated,
@@ -90,10 +103,13 @@ class fantasySpider:
 
 spider= fantasySpider()
 
-def main():
+async def main():
 	start=time.time()
-	print(spider.get_max_mover_and_faller(1,100))
+	result = await spider.get_max_mover_and_faller(1,10000)
+	print(result)
+
 	stop= time.time()- start
 	print(stop)
 
-main()
+
+asyncio.run(main())
--- test.py
+++ test.py
@@ -1,11 +1,47 @@
 import asyncio
+import requests
 import time
+import asyncio
+import httpx
 
-async def func():
-    time.sleep(10)
+manager_history="https://fantasy.premierleague.com/api/entry/{}/history/"
+
+
+start = time.time()
+for i in range(1, 11):
+    r = requests.get(manager_history.format(i))
+    print(r)
+print(time.time() - start)
+
+start = time.time()
+with requests.Session() as s:
+    for i in range(1, 11):
+       r =  s.get(manager_history.format(i))
+       print(r)
+print(time.time() - start)
+
+"""
+async def get_history(id):
+    async with httpx.AsyncClient() as client:
+        return await client.get(manager_history.format(id))
 
 async def main():
-    return asyncio.create_task(func())
-    print("yes")
+    start = time.time()
+    r = await asyncio.gather(map(get_history, range(1,10)))
+    print(time.time() - start)
 
 asyncio.run(main())
+"""
+async def get_async(id):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(manager_history.format(id))
+        return r.text
+
+urls = range(1,11)
+
+async def launch():
+    start = time.time()
+    resps = await asyncio.gather(*map(get_async, urls))
+    print(time.time() - start)
+
+asyncio.run(launch())
